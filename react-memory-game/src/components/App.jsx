eb498533bfb7140ffc2a7b886f9eee3e64d1db1c
Carlos Hernandez Topete <chtopete@gmail.com>
2022-02-24 18:28:10 +0900

    first attempt on REACT

--- react-memory-game/src/components/App.jsx
+++ react-memory-game/src/components/App.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import Card from "./Card";
-import contacts from "../members"
+import members from "../members"
 
 function App () {
-  return (
 
+  return (
+    <div className="memory-card">
+      <Card
+        name={members[0].name}
+        img={members[0].img}
+      />
+      <Card
+        name={members[1].name}
+        img={members[1].img}
+      />
+      <Card
+        name={members[2].name}
+        img={members[2].img}
+      />
+      <Card
+        name={members[3].name}
+        img={members[3].img}
+      />
+      <Card
+        name={members[4].name}
+        img={members[4].img}
+      />
+      <Card
+        name={members[5].name}
+        img={members[5].img}
+      />
+    </div>
   );
 }
 
--- react-memory-game/src/components/Card.jsx
+++ react-memory-game/src/components/Card.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
-function Card () {
+
+function Card(props) {
   return (
-
+  <div className="memory-card">
+      <h2 className="name">{props.name}</h2>
+      <img className="front-face"
+        src={props.img}
+        alt="member_img"
+      />
+      <img className="back-face"
+        src="img/PAW_Patrol_Logo.png"
+        alt="PAW_Patrol_Logo"
+      />
+  </div>
   );
-}
-
 export default Card;
