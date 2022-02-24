4239340ee14d0ac989eb62c33bde8049cb0f0b7d
Carlos Hernandez Topete <chtopete@gmail.com>
2022-02-24 17:31:28 +0900

    members added

---
+++ react-memory-game/src/members.js
@@ -0,0 +1,31 @@
+const members [
+  {
+    name: "Chase",
+    img: "../img/Chase",
+  },
+
+   {
+    name: "Everest",
+    img: "../img/Everest",
+  },
+  {
+    name: "Marshall",
+    img: "../img/Marshall",
+  },
+  {
+    name: "Rocky",
+    img: "../img/Rocky",
+  },
+
+  {
+    name: "Rubble",
+    img: "../img/Rubble",
+  },
+
+  {
+    name: "Skye",
+    img: "../img/Skye",
+  },
+]
+
+export default members;
