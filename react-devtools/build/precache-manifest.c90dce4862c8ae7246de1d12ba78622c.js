f946d0393791b0d85a260a8ee1eda108bd6552b6
Carlos Hernandez Topete <chtopete@gmail.com>
2022-02-28 16:52:51 +0900

    Details component added

---
+++ react-devtools/build/precache-manifest.c90dce4862c8ae7246de1d12ba78622c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe6a427362d9c2c6fbb6721ab61c996",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcf2a5f21878e7a4bb72",
+    "url": "/static/js/2.7bd492ba.chunk.js"
+  },
+  {
+    "revision": "6e154fd0ce36bfef339e",
+    "url": "/static/js/main.c7c2c9b6.chunk.js"
+  },
+  {
+    "revision": "e782b4736e2bd022c8fb",
+    "url": "/static/js/runtime-main.53aae55d.js"
+  }
+]);
