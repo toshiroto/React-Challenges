1a18a6281268b29d28ccf049ad384c32843a0544
Carlos Hernandez Topete <chtopete@gmail.com>
2022-03-01 16:14:02 +0900

    mapping emojis completed

--- Mapping-Emojis/src/components/App.jsx
+++ Mapping-Emojis/src/components/App.jsx
@@ -2,10 +2,6 @@ import React from "react";
 import Entry from "./Entry";
 import emojipedia from "../emojipedia"
 
-//2. Craate Props to replace the hard coded data
-//3a. Import emojipedia <const></const>
-//3. Map through emojipedia array and render entry components
-
 function createEntry(emoji) {
   return (
     <Entry
@@ -24,10 +20,8 @@ function App() {
       <h1>
         <span>emojipedia</span>
       </h1>
-
       <dl className="dictionary">
      {emojipedia.map(createEntry)}
-
       </dl>
     </div>
   );
