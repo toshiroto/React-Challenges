d605b0becec1469e50a0e054edbd3a98ea6c0458
Carlos Hernandez Topete <chtopete@gmail.com>
2022-03-10 14:09:37 +0900

    && expression instead of ternary operator

--- conditional-rendering/src/components/App.jsx
+++ conditional-rendering/src/components/App.jsx
@@ -3,12 +3,14 @@ import Login from "./Login"
 
 let isLoggedIn = false
 
-
+const currentTime = new Date(2022, 3, 10, 18).getHours();
 
 function App() {
   return (
     <div className="container">{
-    isLoggedIn ? <h1>Hello</h1> : <Login />
+   // isLoggedIn ? <h1>Hello</h1> : <Login />
+
+currentTime > 12 && <h1>Why are you still working? </h1>
     } </div>
   );
 }
